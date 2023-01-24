ae532d4a691bc378845c1db6864dade23d672726
Ashura <ashuramajestic@gmail.com>
2023-01-24 15:29:10 +0530

    cool

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.card').addEventListener('mouseover',()=>{
+    document.getElementById('name').innerText="AshuraMajestic";
+    document.getElementById('img').src="img/github.png";
+    document.querySelector('html').style.backgroundColor="white";
+})
+
+document.querySelector('.card').addEventListener('mouseout',()=>{
+    document.getElementById('name').innerText="@ashuramajestic";
+    document.getElementById('img').src="img/insta.png";
+    document.querySelector('html').style.backgroundColor="#1a1a1d";
+})
